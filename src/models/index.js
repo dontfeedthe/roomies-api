e46f7d4571f09799e1ff9496717d69a40d7b6808
Simon Renoult <simon.renoult@gmail.com>
2015-11-03 23:38:16 +0100

    [UTILITY] Add staging database

--- src/models/index.js
+++ src/models/index.js
@@ -4,9 +4,15 @@ var fs = require('fs')
 var path = require('path')
 var Sequelize = require('sequelize')
 var env = process.env.NODE_ENV || 'development'
-var config = require(__dirname + '/../config')[env].db
-var sequelize = new Sequelize(config.database, config.username, config.password, config)
 var db = {}
+
+var sequelize
+if (env === 'staging') {
+  sequelize = new Sequelize(process.env.DATABASE_URL)
+} else {
+  var config = require(__dirname + '/../config')[env].db
+  sequelize = new Sequelize(config.database, config.username, config.password, config)
+}
 
 fs
   .readdirSync(__dirname)
